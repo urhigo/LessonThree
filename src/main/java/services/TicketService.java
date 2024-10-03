b3bacc8a3612724ac436bb05ab73a458caf188ce
Zhuk Egor <zhukegr@gmail.com>
2024-10-03 15:41:49 +0300

    Add ticket storage in TicketService.

--- src/main/java/services/TicketService.java
+++ src/main/java/services/TicketService.java
@@ -1,6 +1,11 @@
 package services;
 
+import models.Sector;
+import models.Ticket;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Stream;
 
 
 public class TicketService {
@@ -30,5 +35,21 @@ public class TicketService {
         } else {
             return LocalDateTime.of(2024, 12, 5, 20, 30);
         }
+    }
+
+    public List<Ticket> ticketsStorage(){
+        Ticket ticket1 = new Ticket ("Palace", (short) 123, Sector.B, true);
+        Ticket ticket2 = new Ticket ("Hall", (short) 223, Sector.A, true);
+        Ticket ticket3 = new Ticket ("Brazil", (short) 111, Sector.C, true);
+        Ticket ticket4 = new Ticket ("Stadium", (short) 234, Sector.C, true);
+        Ticket ticket5 = new Ticket ("Theater", (short) 456, Sector.A, true);
+        Ticket ticket6 = new Ticket ("Theater", (short) 276, Sector.B, true);
+        Ticket ticket7 = new Ticket ("Hall", (short) 987, Sector.B, true);
+        Ticket ticket8 = new Ticket ("Brazil", (short) 345, Sector.A, true);
+        Ticket ticket9 = new Ticket ("Club", (short) 222, Sector.A, true);
+        Ticket ticket10 = new Ticket ("Club", (short) 333, Sector.B, true);
+
+        return Stream.of(ticket1, ticket2, ticket3, ticket4, ticket5,
+                ticket6, ticket7, ticket8, ticket9, ticket10).toList();
     }
 }
