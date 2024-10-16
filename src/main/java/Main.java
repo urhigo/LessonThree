ce6c92c93cffa7b53f4ec3cec666523777a4877c
Zhuk Egor <zhukegr@gmail.com>
2024-10-16 15:29:37 +0300

    Create my own ArrayList.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import models.Ticket;
+import services.MyArrayList;
 import services.TicketService;
 
 import java.util.List;
@@ -10,5 +11,16 @@ public class Main {
         List<Ticket> listTickets = new TicketService().ticketsStorage();
         new TicketService().getInformationAboutTicketById((short) 1, listTickets);
         new TicketService().getInformationAboutTicketById((short) 44, listTickets);
+
+        MyArrayList<String> listDays = new MyArrayList<>();
+        listDays.add("One");
+        listDays.add("Two");
+        listDays.add("Three");
+
+        System.out.println(listDays.get(2));
+
+        listDays.remove(1);
+
+        System.out.println(listDays.get(1));
     }
 }
