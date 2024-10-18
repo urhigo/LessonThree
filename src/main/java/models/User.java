cd753391108a697bf014270a9ef850becbae00fb
Zhuk Egor <zhukegr@gmail.com>
2024-10-18 14:45:01 +0300

    Create DAO class UserDAO.

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -3,11 +3,14 @@ package models;
 import Interface.Printable;
 import services.BaseIdGeneratingEntity;
 
+import java.time.LocalDateTime;
+
 public class User extends BaseIdGeneratingEntity implements Printable {
 
 
     private String name;
     private Role role;
+    private final LocalDateTime timeCreateUser = LocalDateTime.now();
 
     public User() {
     }
@@ -21,7 +24,17 @@ public class User extends BaseIdGeneratingEntity implements Printable {
         return role;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getTimeCreateUser() {
+        return timeCreateUser;
+    }
 
     @Override
     public void printInformationAboutObject(Object object) {
