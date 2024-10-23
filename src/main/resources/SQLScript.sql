01732aee229e00b037106d6c379ab4fd07465a19
Zhuk Egor <zhukegr@gmail.com>
2024-10-24 00:34:01 +0300

    Creation script

---
+++ src/main/resources/SQLScript.sql
@@ -0,0 +1,13 @@
+CREATE TABLE _user (
+id VARCHAR(50),
+name VARCHAR(50),
+creation_table VARCHAR(50)
+);
+
+CREATE TABLE ticket(
+id VARCHAR(50),
+user_id VARCHAR(50),
+ticket_type VARCHAR(50),
+creation_table VARCHAR(50)
+)
+
