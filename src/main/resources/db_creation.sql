a837948564c9634ae92dd1af21288e5d021676da
Zhuk Egor <zhukegr@gmail.com>
2024-10-31 13:37:06 +0300

    Correct my sql script

--- src/main/resources/db_creation.sql
+++ src/main/resources/db_creation.sql
@@ -9,7 +9,7 @@ creation_table VARCHAR(50)
 CREATE TABLE ticket (
 id VARCHAR(50) PRIMARY KEY,
 user_id VARCHAR(50),
-ticket_type ticket_type,
+ticket_type ticket_type NOT NULL,
 creation_table VARCHAR(50),
 CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES _user(id) ON DELETE CASCADE
 );
